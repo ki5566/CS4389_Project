98bc52c714d0443615c7b635eb79cfdb71c0afdd
Linh <linhnha3@gmail.com>
2025-11-16 12:46:50 -0600

    Updated dashboard metrics, charts, & layout

--- dashboard.py
+++ dashboard.py
@@ -1,23 +1,68 @@
 import streamlit as st
 import query
+import pandas as pd
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
 transactions = query.get_transactions()
+transactions['Value (wei)'] = transactions['Value'].apply(lambda x: f'{x:.3E}') # Value formatted in scientific notation
+transactions['Date'] = transactions['Timestamp'].dt.date
+transactions['Hour'] = transactions['Timestamp'].dt.hour
+
 tx_count = query.get_tx_count()
 block_count = query.get_block_count()
 avg_value = query.get_avg_value()
 
-if  tx_count:
-    st.metric(label='TOTAL TRANSACTIONS', value=tx_count, border=True, width='content')
+with st.container(horizontal=True):
 
-if  block_count:
-    st.metric(label='TOTAL BLOCKS', value=block_count, border=True, width='content')
+    # For metrics
+    with st.container(horizontal=True, horizontal_alignment='distribute', border=False, width=460):
+        if  tx_count:
+            st.metric(label='TOTAL TRANSACTIONS', value=tx_count, border=True, width='content')
+        if  block_count:
+            st.metric(label='TOTAL BLOCKS', value=block_count, border=True, width='content')
+        st.metric(label='TOTAL ALERTS', value='10', border=True, width='content')
+        if  avg_value:
+            st.metric(label='AVERAGE TRANSACTION VALUE', value=f'{avg_value:.3E} wei', border=True, width='content')
+        st.metric(label='SUSPICIOUS TRANSACTIONS', value='10%', border=True, width='content')
+        
+    # Add container for alerts table here
 
-if  avg_value:
-    st.metric(label='AVERAGE TRANSACTION VALUE', value=f'{avg_value} wei', border=True, width='content')
 
 if not transactions.empty:
-    st.line_chart(transactions.head(1000), x='Timestamp', y='Value (wei)', color='#32d7ed')
+    with st.container(horizontal=True, horizontal_alignment='distribute'):
+        line, pie = st.columns([0.65, 0.35], gap='large')
+        
+        with line:
+            grouped = (
+                transactions.groupby(['Date', 'Hour'])['Value']
+                .mean()
+                .reset_index()
+                .sort_values(['Date', 'Hour'])
+            )
+            
+            unique_dates = grouped['Date'].unique()
+            chart_tabs = st.tabs([str(day) for day in unique_dates])
+
+            for tab, day in zip(chart_tabs, unique_dates):
+                with tab:
+                    day_df = grouped[grouped['Date'] == day]
+                    st.subheader(f"Average Transaction Value on {day}", anchor=False)
+                    chart_df = day_df.set_index('Hour')['Value']
+                    
+                    st.line_chart(chart_df, x_label='Hour of Day', y_label='Average Value')
     
-    st.dataframe(data=transactions, hide_index=True)
+        with pie:
+            st.subheader("Breakdown of Alert Types", anchor=False, divider='blue')
+            # Sample data
+            alert_df = pd.DataFrame({
+                "Alert": ["Chain", "Account", "Spam"],
+                "Count": [10, 4, 6]
+            })
+            fig = px.pie(alert_df, names="Alert", values="Count")
+            st.plotly_chart(fig)
+
+
+    columns = ['Transaction Hash', 'Value (wei)', 'From', 'To', 'Timestamp']
+    st.dataframe(data=transactions[columns], hide_index=True)
--- query.py
+++ query.py
@@ -1,6 +1,11 @@
 import sqlite3
 import pandas as pd
 
+def value_to_int(val):
+    if isinstance(val, str) and val.startswith('0x'):
+        return int(val, 16)
+    return val
+
 def get_transactions():
     conn = sqlite3.connect('blockchain.db')
     cur = conn.cursor()
@@ -11,10 +16,10 @@ def get_transactions():
                 """)
     
     transactions = cur.fetchall()
-    column_names = ['Transaction Hash', 'Value (wei)', 'From', 'To', 'Block', 'Timestamp']
+    column_names = ['Transaction Hash', 'Value', 'From', 'To', 'Block', 'Timestamp']
     df = pd.DataFrame(data=transactions, columns=column_names)
     df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
-
+    df['Value'] = df['Value'].apply(lambda x: value_to_int(x))
     conn.close()
     return df
 
